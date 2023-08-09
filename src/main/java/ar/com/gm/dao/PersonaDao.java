fbdde8805309a3c1a7cc52f22f4d995e1b412ee9
Acosta, Federico <107961278+AcostaF12@users.noreply.github.com>
2023-08-09 00:12:54 -0300

    inicio proyecto, logica terminada.

---
+++ src/main/java/ar/com/gm/dao/PersonaDao.java
@@ -0,0 +1,9 @@
+package ar.com.gm.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.com.gm.domain.Persona;
+
+public interface PersonaDao extends CrudRepository<Persona, Long>{
+    
+}
