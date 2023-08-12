d2eb0c100fc53cc609f02e22223a3a31bd3c0f9f
Acosta, Federico <107961278+AcostaF12@users.noreply.github.com>
2023-08-12 18:47:36 -0300

    Refactor

--- src/main/java/ar/com/gm/dao/PersonaDao.java
+++
@@ -1,9 +0,0 @@
-package ar.com.gm.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ar.com.gm.domain.Persona;
-
-public interface PersonaDao extends CrudRepository<Persona, Long>{
-    
-}
